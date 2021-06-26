0294b37992439a0d4081c61f9fcfa88c81be45b8
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-26 16:48:28 -0300

    feat: list and create tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string
+  ) {
+    const task = this.tasksService.create(title, description);
+
+    return task;
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
+
+import { Task, TaskStatus } from './tasks.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  findAll() {
+    return this.tasks;
+  }
+
+  create(title: string, description: string): Task {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
