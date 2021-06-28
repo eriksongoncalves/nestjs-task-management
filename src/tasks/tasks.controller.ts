c9611b92bb9c39a9cf8613b77f5ef5a5ce9d1897
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-28 11:43:21 -0300

    feat: finishing crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  HttpStatus,
+  Delete,
+  Patch,
+  Query
+} from '@nestjs/common';
+import { Response } from 'express';
+
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,17 +22,47 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  findAll() {
-    return this.tasksService.findAll();
+  findAll(@Query() filtersDto: GetTasksFilterDto) {
+    return this.tasksService.findAll(filtersDto);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string, @Res() res: Response) {
+    const task = this.tasksService.findById(id);
+
+    if (task) {
+      return res.json(task);
+    }
+
+    return res.status(HttpStatus.NOT_FOUND).json({ error: 'Task not found' });
   }
 
   @Post()
-  create(
-    @Body('title') title: string,
-    @Body('description') description: string
-  ) {
-    const task = this.tasksService.create(title, description);
+  create(@Body() createTaskDto: CreateTaskDto, @Res() res: Response) {
+    const task = this.tasksService.create(createTaskDto);
 
-    return task;
+    return res.status(HttpStatus.CREATED).json(task);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string, @Res() res: Response) {
+    this.tasksService.delete(id);
+
+    return res.status(HttpStatus.NO_CONTENT).json({});
+  }
+
+  @Patch('/:id/status')
+  updateStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+    @Res() res: Response
+  ) {
+    const task = this.tasksService.updateStatus(id, status);
+
+    if (task) {
+      return res.json(task);
+    }
+
+    return res.status(HttpStatus.NOT_FOUND).json({ error: 'Task not found' });
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  findAll() {
-    return this.tasks;
+  findAll(filtersDto: GetTasksFilterDto) {
+    let tasksFiltered = this.tasks;
+
+    if (filtersDto.status) {
+      tasksFiltered = this.tasks.filter(
+        task => task.status === filtersDto.status
+      );
+    }
+
+    if (filtersDto.search) {
+      tasksFiltered = tasksFiltered.filter(
+        task =>
+          task.title.includes(filtersDto.search) ||
+          task.description.includes(filtersDto.search)
+      );
+    }
+
+    return tasksFiltered;
   }
 
-  create(title: string, description: string): Task {
+  findById(id: string) {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  create({ title, description }: CreateTaskDto): Task {
     const task = {
       id: v4(),
       title,
@@ -22,5 +44,21 @@ export class TasksService {
     this.tasks.push(task);
 
     return task;
+  }
+
+  delete(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  updateStatus(id: string, status: TaskStatus): Task | null {
+    const task = this.findById(id);
+
+    if (task) {
+      task.status = status;
+
+      return task;
+    }
+
+    return null;
   }
 }
